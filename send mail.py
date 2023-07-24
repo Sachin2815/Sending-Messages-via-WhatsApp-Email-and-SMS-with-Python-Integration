eb04c050a91e61991924938e57ef63d1258a557a
Sachin Singh <100368589+Sachin2815@users.noreply.github.com>
2023-07-24 11:15:23 +0530

    Update send mail.py

--- send mail.py
+++ send mail.py
@@ -1,42 +1,44 @@
-#1. Importing the necessary libraries:
 import smtplib
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 
-#2. Defining the send_email function:
-def send_email(sender_email, sender_password, receiver_email, subject, message):
-    # 3. Create a multipart message and set headers
-    email_message = MIMEMultipart()
-    email_message['From'] = sender_email
-    email_message['To'] = receiver_email
-    email_message['Subject'] = subject
+def send_email(sender_email, sender_password, receiver_email, subject, body):
+    try:
+        # Set up the SMTP server
+        smtp_server = "smtp.gmail.com"
+        port = 587  # Use 465 for SSL or 587 for TLS
+        smtp = smtplib.SMTP(smtp_server, port)
+        smtp.starttls()  # Encrypt the connection
 
+        # Login to your email account
+        smtp.login(sender_email, sender_password)
 
-    # 4. Add body to the email
-    email_message.attach(MIMEText(message, 'plain'))
+        # Create the email message
+        message = MIMEMultipart()
+        message["From"] = sender_email
+        message["To"] = receiver_email
+        message["Subject"] = subject
 
+        # Attach the body to the email
+        message.attach(MIMEText(body, "plain"))
 
-    # 5. Create SMTP session for sending the email
-    smtp_server = smtplib.SMTP('smtp.gmail.com', 587)
-    smtp_server.starttls()
-    smtp_server.login(sender_email, sender_password)
+        # Send the email
+        smtp.sendmail(sender_email, receiver_email, message.as_string())
 
-    # Convert the multipart message to a string
-    email_text = email_message.as_string()
+        # Quit the SMTP server
+        smtp.quit()
 
+        print("Email sent successfully!")
 
-    # Send the email
-    smtp_server.sendmail(sender_email, receiver_email, email_text)
-    smtp_server.quit()
+    except Exception as e:
+        print(f"Error: {str(e)}")
 
+if __name__ == "__main__":
+    # Replace these with your actual email credentials and email content
+    sender_email = "sachinsingh58624@gmail.com"
+    sender_password = "password"
+    receiver_email = "mayank07082001@gmail.com"
+    subject = "Test Email from Python"
+    body = "This is a test email sent from Python."
 
-# Example usage
-sender_email = 'sachinsingh58624@gmail.com'
-sender_password = 'password'
-receiver_email = 'sachin.singh.professional@gmail.com'
-subject = 'Hello from sachin-!'
-message = "I hope you are well ,  let's meet soon."
-
-
-send_email(sender_email, sender_password, receiver_email, subject, message)
-
+    send_email(sender_email, sender_password, receiver_email, subject, body)
